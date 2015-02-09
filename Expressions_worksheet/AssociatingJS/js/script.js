1e824193a65697793b4c6d8f4af5f7fbb760d635
mrsdivastylz <mrs_gmanning@hotmail.com>
2015-02-09 13:49:26 -0500

    This Dog years exercise
    This is my code for Dog years in Java script.

--- Expressions_worksheet/AssociatingJS/js/script.js
+++ Expressions_worksheet/AssociatingJS/js/script.js
@@ -15,6 +15,6 @@ var result;
 sparkyAge = 3; // The age sparky is in human years
 prompt("How old is Sparky in human years?"); //This is the prompt that asks users how old Sparky is in human years.
 dogYears = sparkyAge * 7; // This is how the expression is calculated to answer how old Sparky is in dog years
-result = ("Sparky is" + sparkyAge + "human years old which is" + dogYears + "in dog years."); //This is the results from calculation from human years to dog years.
+result = ("Sparky is " + sparkyAge + " human years old which is " + dogYears + " in dog years."); //This is the results from calculation from human years to dog years.
 alert(result); //This is the calculation of what the user sees on their screen.
 console.log(result); //This is what is logged into the console
