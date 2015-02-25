c12b772ebbe7a08c03622c8688bfa252d19e4211
mrsdivastylz <mrs_gmanning@hotmail.com>
2015-02-25 18:59:32 -0500

    Functions worksheet7
    I had to move the var calculatedCirumferenceOfCirlce to the end of code
    after  function in order to invoke it. When I had it at the top with
    variables I got an uncaught  reference error. But it is fixed now and
    now it prints to console.

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -11,15 +11,14 @@ alert("JavaScript Works");
 //Variables
 var radius = 5;                             //This is the radius of a circle
 var pi = 3.14;                              // This is the pi of the circle
-var circumference = calulatedCircumferenceOfCircle(radius); //function that calculates the circumference.
 
 //Parameters for function
-function calculatedCircumferenceOFCircle(radius) //defining the function name and setting the parameter
+function calculatedCircumferenceOfCircle(radius) //defining the function name and setting the parameter
 {
-var circumference;                       //declaring my variable for the function.
+    var circumference;                       //declaring my variable for the function.
     circumference = 3.14 * (radius * 2); // This is the code that the function will run.
 
     return circumference;               // This is how I return the circumference back to the function.
-    console.log("The circumference of the circle is " + circumference + "."); //This is the result that I will print to console.
 }
+var circumference = calculatedCircumferenceOfCircle(radius)
 console.log("The circumference of the circle is  " + circumference); //The result that will print to console.
