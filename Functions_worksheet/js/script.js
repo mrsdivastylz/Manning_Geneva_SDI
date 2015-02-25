c48c9e866ecf99a71f00a1ba7d82cabb841d9342
mrsdivastylz <mrs_gmanning@hotmail.com>
2015-02-25 18:46:03 -0500

    Functions worksheet6
    I added the variables outside of function.

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -8,6 +8,10 @@ alert("JavaScript Works");
 //Circumference
 //Calculate the circumference of a circle.
 
+//Variables
+var radius = 5;                             //This is the radius of a circle
+var pi = 3.14;                              // This is the pi of the circle
+var circumference = calulatedCircumferenceOfCircle(radius); //function that calculates the circumference.
 
 //Parameters for function
 function calculatedCircumferenceOFCircle(radius) //defining the function name and setting the parameter
@@ -16,6 +20,6 @@ var circumference;                       //declaring my variable for the functio
     circumference = 3.14 * (radius * 2); // This is the code that the function will run.
 
     return circumference;               // This is how I return the circumference back to the function.
-    console.log("This circumferece of the circle is " + circumference + "."); //This is the result that I will print to console.
+    console.log("The circumference of the circle is " + circumference + "."); //This is the result that I will print to console.
 }
-
+console.log("The circumference of the circle is  " + circumference); //The result that will print to console.
