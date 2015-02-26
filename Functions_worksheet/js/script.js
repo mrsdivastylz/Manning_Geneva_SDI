31cb2d003965204b935f733c7abb5e5c035ec44e
mrsdivastylz <mrs_gmanning@hotmail.com>
2015-02-25 19:25:03 -0500

    Functions worksheet9
    Declaring the variables for the Stung assignment.

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -22,3 +22,11 @@ function calculatedCircumferenceOfCircle(radius) //defining the function name an
 }
 var circumference = calculatedCircumferenceOfCircle(radius)//This is where I invoke my function.
 console.log("The circumference of the circle is  " + circumference); //The result that will print to console.
+
+//Stung
+//It takes 8.666666667 bee stings per pound to kill an animal. Calculate how many bee stings are needed to kill an animal in a function.
+
+//variables
+
+var victimWeight = prompt("Please enter a number to that will represent how many lbs the animal is.");
+var beeStings = 8.666666667;
